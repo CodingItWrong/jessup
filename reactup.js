f60670616b263e8b974fea4560911bf9eb60381e
Josh Justice <me@codingitwrong.com>
2022-05-12 16:42:04 -0400

    Add hello world prompt

--- reactup.js
+++ reactup.js
@@ -1,1 +1,14 @@
-console.log('hello, reactup');
+const inquirer = require('inquirer');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    default: 'Josh',
+  },
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hello, ${answers.name}!`);
+});
