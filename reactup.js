70e7d93b738dbe06383138cf96b7f7907fd9f367
Josh Justice <me@codingitwrong.com>
2022-05-12 16:45:21 -0400

    Prompt for framework

--- reactup.js
+++ reactup.js
@@ -2,16 +2,24 @@ const inquirer = require('inquirer');
 
 const questions = [
   {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
-    default: 'Josh',
+    type: 'list',
+    name: 'framework',
+    message: 'What kind of app do you want to create?',
+    choices: [
+      {value: 'cra', name: 'Create React App'},
+      {value: 'doc', name: 'Docusaurus'},
+      {value: 'expo', name: 'Expo'},
+      {value: 'next', name: 'Next'},
+      {value: 'node', name: 'Node'},
+      {value: 'babel', name: 'Node with Babel'},
+      {value: 'rn', name: 'React Native CLI'},
+    ],
   },
 ];
 
 async function run() {
   const answers = await inquirer.prompt(questions);
-  console.log(`Hello, ${answers.name}!`);
+  console.log({answers});
 }
 
 run();
