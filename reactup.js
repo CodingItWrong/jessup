0e8d8a6a9f6e6da22b1d250a1f1430d0f9e401a7
Josh Justice <me@codingitwrong.com>
2022-05-13 06:51:48 -0400

    Put initializers in map

--- reactup.js
+++ reactup.js
@@ -1,11 +1,125 @@
 const inquirer = require('inquirer');
 
+function runCra(answers) {
+  commit('Initialize project', () => {
+    command(`yarn create react-app ${answers.projectName}`);
+    command(`cd ${answers.projectName}`);
+    command('git init .');
+  });
+
+  commit('Prevent package lock', () => {
+    command('echo "package-lock=false" >> .npmrc');
+  });
+
+  commit('Configure linting and formatting', () => {
+    addNpmPackages({
+      dev: true,
+      packages: [
+        'eslint-config-prettier',
+        'eslint-plugin-prettier',
+        'prettier',
+      ],
+    });
+    writeFile(
+      '.eslintrc.js',
+      `
+        module.exports = {
+          extends: ['react-app', 'prettier'],
+          plugins: ['prettier'],
+          rules: {
+            'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+            'no-duplicate-imports': 'error',
+            'prettier/prettier': 'warn',
+            quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
+            'sort-imports': [
+              'warn',
+              {ignoreDeclarationSort: true, ignoreMemberSort: false},
+            ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+          },
+        };
+      `
+    );
+    writeFile(
+      '.prettierrc.js',
+      `
+          module.exports = {
+            arrowParens: 'avoid',
+            bracketSpacing: false,
+            singleQuote: true,
+            trailingComma: 'es5',
+          };
+        `
+    );
+    command('npm set-script lint "eslint ."');
+    command('yarn lint --fix');
+  });
+}
+
+function runNode(answers) {
+  commit('Initialize project', () => {
+    command(`mkdir ${answers.projectName}`);
+    command(`cd ${answers.projectName}`);
+    command('yarn init -y');
+    command('npx gitignore node');
+    command('git init .');
+  });
+
+  commit('Prevent package lock', () => {
+    command('echo "package-lock=false" >> .npmrc');
+  });
+
+  commit('Configure linting and formatting', () => {
+    addNpmPackages({
+      dev: true,
+      packages: [
+        'eslint',
+        'eslint-config-prettier',
+        'eslint-plugin-prettier',
+        'prettier',
+      ],
+    });
+    writeFile(
+      '.eslintrc.js',
+      `
+        module.exports = {
+          extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+          plugins: ['jest'],
+          env: {
+            es6: true,
+            'jest/globals': true,
+            node: true,
+          },
+          parserOptions: {
+            ecmaVersion: 'latest',
+          },
+          rules: {
+            quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
+          },
+        };
+      `
+    );
+    writeFile(
+      '.prettierrc.js',
+      `
+        module.exports = {
+          arrowParens: 'avoid',
+          bracketSpacing: false,
+          singleQuote: true,
+          trailingComma: 'es5',
+        };
+      `
+    );
+    command('npm set-script lint "eslint ."');
+    command('yarn lint --fix');
+  });
+}
+
 const FRAMEWORKS = [
-  {value: 'cra', name: 'Create React App'},
+  {value: 'cra', name: 'Create React App', initializer: runCra},
   {value: 'doc', name: 'Docusaurus'},
   {value: 'expo', name: 'Expo'},
   {value: 'next', name: 'Next'},
-  {value: 'node', name: 'Node'},
+  {value: 'node', name: 'Node', initializer: runNode},
   {value: 'babel', name: 'Node with Babel'},
   {value: 'rn', name: 'React Native CLI'},
 ];
@@ -57,116 +171,7 @@ async function run() {
 
   console.log(`Initializing ${framework.name} project`);
 
-  if (answers.framework === 'cra') {
-    commit('Initialize project', () => {
-      command(`yarn create react-app ${answers.projectName}`);
-      command(`cd ${answers.projectName}`);
-      command('git init .');
-    });
-
-    commit('Prevent package lock', () => {
-      command('echo "package-lock=false" >> .npmrc');
-    });
-
-    commit('Configure linting and formatting', () => {
-      addNpmPackages({
-        dev: true,
-        packages: [
-          'eslint-config-prettier',
-          'eslint-plugin-prettier',
-          'prettier',
-        ],
-      });
-      writeFile(
-        '.eslintrc.js',
-        `
-          module.exports = {
-            extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-            env: {
-              es6: true,
-              node: true,
-            },
-            parserOptions: {
-              ecmaVersion: 'latest',
-            },
-            rules: {
-              quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
-            },
-          };
-        `
-      );
-      writeFile(
-        '.prettierrc.js',
-        `
-          module.exports = {
-            arrowParens: 'avoid',
-            bracketSpacing: false,
-            singleQuote: true,
-            trailingComma: 'es5',
-          };
-        `
-      );
-      command('npm set-script lint "eslint ."');
-      command('yarn lint --fix');
-    });
-  } else {
-    // node
-    commit('Initialize project', () => {
-      command(`mkdir ${answers.projectName}`);
-      command(`cd ${answers.projectName}`);
-      command('yarn init -y');
-      command('npx gitignore node');
-      command('git init .');
-    });
-
-    commit('Prevent package lock', () => {
-      command('echo "package-lock=false" >> .npmrc');
-    });
-
-    commit('Configure linting and formatting', () => {
-      addNpmPackages({
-        dev: true,
-        packages: [
-          'eslint',
-          'eslint-config-prettier',
-          'eslint-plugin-prettier',
-          'prettier',
-        ],
-      });
-      writeFile(
-        '.eslintrc.js',
-        `
-          module.exports = {
-            extends: ['react-app', 'prettier'],
-            plugins: ['prettier'],
-            rules: {
-              'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
-              'no-duplicate-imports': 'error',
-              'prettier/prettier': 'warn',
-              quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
-              'sort-imports': [
-                'warn',
-                {ignoreDeclarationSort: true, ignoreMemberSort: false},
-              ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
-            },
-          };
-        `
-      );
-      writeFile(
-        '.prettierrc.js',
-        `
-        module.exports = {
-          arrowParens: 'avoid',
-          bracketSpacing: false,
-          singleQuote: true,
-          trailingComma: 'es5',
-        };
-      `
-      );
-      command('npm set-script lint "eslint ."');
-      command('yarn lint --fix');
-    });
-  }
+  framework.initializer(answers);
 }
 
 run();
