712f39898f330c136be1272161af254e397fecb5
Josh Justice <me@codingitwrong.com>
2022-05-13 10:17:54 -0400

    Expo sample code

--- frameworks.js
+++ frameworks.js
@@ -180,6 +180,45 @@ function initializeExpo(answers) {
     setScript('lint', 'eslint .');
     command('yarn lint --fix');
   });
+
+  group('Create sample files', () => {
+    writeFile(
+      'App.js',
+      `
+        import React from 'react';
+        import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+        export default function App() {
+          return (
+            <>
+              <StatusBar barStyle="dark-content" />
+              <SafeAreaView>
+                <Text>Hello, React Native!</Text>
+              </SafeAreaView>
+            </>
+          );
+        }
+      `
+    );
+
+    if (answers.unitTesting) {
+      writeFile(
+        'App.spec.js',
+        `
+          import {render} from '@testing-library/react-native';
+          import React from 'react';
+          import App from './App';
+
+          describe('App', () => {
+            it('renders a hello message', () => {
+              const {queryByText} = render(<App />);
+              expect(queryByText('Hello, React Native!')).not.toBeNull();
+            });
+          });
+        `
+      );
+    }
+  });
 }
 
 function initializeNode(answers) {
