b6f72574350de01ff3b2f44597dd348cee2aebc6
Josh Justice <me@codingitwrong.com>
2022-05-14 16:32:57 -0400

    Add Node with Babel unit tests

--- gitHubActions.spec.js
+++ gitHubActions.spec.js
@@ -14,4 +14,18 @@ describe('getGitHubActionsConfig', () => {
       ).toMatchSnapshot();
     });
   });
+
+  describe('node with babel', () => {
+    it('can generate a config without unit testing', () => {
+      expect(
+        getGitHubActionsConfig({framework: 'babel', unitTesting: false})
+      ).toMatchSnapshot();
+    });
+
+    it('can generate a config with unit testing', () => {
+      expect(
+        getGitHubActionsConfig({framework: 'babel', unitTesting: true})
+      ).toMatchSnapshot();
+    });
+  });
 });
