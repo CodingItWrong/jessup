8a8c5f18e1a891a1182fcc69cc919e8fe19a318b
Josh Justice <me@codingitwrong.com>
2022-05-15 07:00:55 -0400

    Move everything framework-specific into scripts file

--- jessup.js
+++ jessup.js
@@ -1,58 +1,9 @@
 const inquirer = require('inquirer');
-const FRAMEWORKS = require('./frameworks');
-
-const questions = [
-  {
-    type: 'list',
-    name: 'framework',
-    message: 'What kind of app do you want to create?',
-    choices: FRAMEWORKS,
-  },
-  {
-    type: 'input',
-    name: 'projectName',
-    message: 'What name do you want to give the project?',
-    default: answers =>
-      frameworkForAnswers(answers).defaultProjectName ??
-      `my-${answers.framework}-project`,
-  },
-  {
-    type: 'confirm',
-    name: 'unitTesting',
-    message: 'Configure unit testing?',
-    when: answers => !frameworkForAnswers(answers).skipUnitTestingQuestion,
-  },
-  {
-    type: 'confirm',
-    name: 'cypress',
-    message: 'Configure E2E web testing with Cypress?',
-    when: answers => frameworkForAnswers(answers).cypressAvailable,
-  },
-  {
-    type: 'confirm',
-    name: 'detox',
-    message: 'Configure E2E native testing with Detox?',
-    when: answers => frameworkForAnswers(answers).detoxAvailable,
-  },
-  {
-    type: 'confirm',
-    name: 'gitHubActions',
-    message: 'Configure CI with GitHub Actions?',
-  },
-];
-
-function frameworkForAnswers(answers) {
-  return FRAMEWORKS.find(f => f.value === answers.framework);
-}
+const {questions, initialize} = require('./script');
 
 async function run() {
   const answers = await inquirer.prompt(questions);
-
-  const framework = frameworkForAnswers(answers);
-
-  console.log(`Initializing ${framework.name} project`);
-
-  framework.initializer(answers);
+  initialize(answers);
 }
 
 run();
--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const {
 const {getReadmeContents} = require('./readme');
 const {getGitHubActionsConfig} = require('./gitHubActions');
 
+function frameworkForAnswers(answers) {
+  return FRAMEWORKS.find(f => f.value === answers.framework);
+}
+
 function initializeCra(answers) {
   group(
     'Initialize project',
@@ -862,4 +866,52 @@ const FRAMEWORKS = [
   },
 ];
 
-module.exports = FRAMEWORKS;
+const questions = [
+  {
+    type: 'list',
+    name: 'framework',
+    message: 'What kind of app do you want to create?',
+    choices: FRAMEWORKS,
+  },
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What name do you want to give the project?',
+    default: answers =>
+      frameworkForAnswers(answers).defaultProjectName ??
+      `my-${answers.framework}-project`,
+  },
+  {
+    type: 'confirm',
+    name: 'unitTesting',
+    message: 'Configure unit testing?',
+    when: answers => !frameworkForAnswers(answers).skipUnitTestingQuestion,
+  },
+  {
+    type: 'confirm',
+    name: 'cypress',
+    message: 'Configure E2E web testing with Cypress?',
+    when: answers => frameworkForAnswers(answers).cypressAvailable,
+  },
+  {
+    type: 'confirm',
+    name: 'detox',
+    message: 'Configure E2E native testing with Detox?',
+    when: answers => frameworkForAnswers(answers).detoxAvailable,
+  },
+  {
+    type: 'confirm',
+    name: 'gitHubActions',
+    message: 'Configure CI with GitHub Actions?',
+  },
+];
+
+function initialize(answers) {
+  const framework = frameworkForAnswers(answers);
+
+  console.log(`Initializing ${framework.name} project`);
+
+  framework.initializer(answers);
+}
+
+module.exports = {questions, initialize};
