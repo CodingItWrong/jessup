cba8e92483b040b7a76ab630c433809215cdf529
Josh Justice <me@codingitwrong.com>
2022-07-21 06:44:29 -0400

    Add Screen API to RN tests

--- script.js
+++ script.js
@@ -874,14 +874,14 @@ function writeSampleReactNativeFiles(answers) {
     writeFile(
       'App.spec.js',
       `
-        import {render} from '@testing-library/react-native';
+        import {render, screen} from '@testing-library/react-native';
         import React from 'react';
         import App from './App';
 
         describe('App', () => {
           it('renders a hello message', () => {
-            const {queryByText} = render(<App />);
-            expect(queryByText('Hello, React Native!')).not.toBeNull();
+            render(<App />);
+            expect(screen.queryByText('Hello, React Native!')).not.toBeNull();
           });
         });
       `
